941b047522e7ad3c36cced054477cade2e3986ee
ximlor <2705833399@qq.com>
2016-09-04 10:57:32 +0800

    add switch

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
-  var containter = document.getElementsByTagName('div')[0];
+  var containter = document.getElementsByTagName('ul')[0];
+  containter.innerHTML = '';
   chrome.management.getAll(function(result) {
     for( var i = 0; i < result.length; i++) {
-      var name = result[i]['shortName'] || result[i]['name'];
-      var status = result[i].enabled;
-      var div = document.createElement('div');
-      div.innerHTML = name + ': ' + status;
-      containter.appendChild(div);
+      var checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.id = result[i]['id'];
+      checkbox.checked = result[i]['enabled'];
+      checkbox.addEventListener('click', function() {
+        __this = this;
+        chrome.management.setEnabled(__this.id, __this.checked, function() {
+          init();
+        });
+      });
+      var name = document.createTextNode(result[i]['shortName'] || result[i]['name']);
+      var li = document.createElement('li');
+      li.appendChild(checkbox);
+      li.appendChild(name);
+      containter.appendChild(li);
     }
   });
 }
